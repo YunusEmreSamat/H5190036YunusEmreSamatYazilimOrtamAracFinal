b78a8761f2be9c99e929233474adf6d44559f2d6
YunusEmreSamat <yunusemresamat34@gmail.com>
2021-06-11 13:51:19 +0300

    Commit

--- H5190036YunusEmreSamatFinal/src/main/java/com/medipol/yazilimaraclarifinal/H5190036YunusEmreSamatFinal/UrunRestApi.java
+++ H5190036YunusEmreSamatFinal/src/main/java/com/medipol/yazilimaraclarifinal/H5190036YunusEmreSamatFinal/UrunRestApi.java
@@ -22,6 +22,6 @@ public class UrunRestApi {
 	public  List<Urun> sil(@RequestBody Urun urun){
 		List<Urun> urunliste = UrunDepo.urunSil(urun.getAd(),urun.getFiyat());
 		return urunliste;
-	}
+	} 
 
 }
